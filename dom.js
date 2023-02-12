46b5cacbf74058d7adba9e6af02a5a12a3fcde4b
atiqur <atiqurrahman.cse2552@gmail.com>
2023-02-12 14:26:11 +0600

    button update

--- dom.js
+++ dom.js
@@ -4,10 +4,9 @@ let errorMsg=document.getElementById('error-msg')
 let assignElement=document.getElementById('listGroup')
 
 
-
 // when form submit button click 
 form.addEventListener('submit',function(e){
-    // default behabeher off  kore 
+    // default behavior off  kore 
     e.preventDefault();
 
     formValidation()
@@ -19,35 +18,37 @@ function formValidation(){
         errorMsg.style.color='red'
     }else{
       errorMsg.innerText=""
+      let submitBtn=document.getElementById('submit_btn')
+      submitBtn.innerText="POST"
+
         acceptData()
     }
 }
 
-function createElement(data){
-    // element er modde ja ache tar sathe ei ta add hobe 
-    assignElement.innerHTML +=`
-    <li class="list-group-item d-flex justify-content-between">
-    ${ data}
-    <span class="">
-      <i  onClick="EditPost(this)" class="fa-solid fa-pen-to-square pe-2"></i>
-      <i  onClick="deletePost(this)" class="fa-solid fa-trash"></i>
-    </span>
-  </li>
-    `
-    input.value=''
-}
 
 function acceptData(){
     data=input.value
     createElement(data)
-
-
 }
+
+function createElement(data){
+  // element er modde ja ache tar sathe ei ta add hobe 
+  assignElement.innerHTML +=`
+  <li class="list-group-item d-flex justify-content-between">
+  ${ data}
+  <span class="">
+    <i  onClick="EditPost(this)" class="fa-solid fa-pen-to-square pe-2"></i>
+    <i  onClick="deletePost(this)" class="fa-solid fa-trash"></i>
+  </span>
+</li>
+  `
+  input.value=''
+}
+
 
 // delete element 
 
  function deletePost (e)  {
-    console.log(e)
     e.parentElement.parentElement.remove();
   };
   
@@ -57,6 +58,13 @@ function acceptData(){
 function EditPost (e)  {
     input.value=e.parentElement.parentElement.innerText
     e.parentElement.parentElement.remove()
+
+    
+    // style  apply 
+    let submitBtn=document.getElementById('submit_btn')
+    submitBtn.innerText="update"
+    submitBtn.style.backgroundColor='#1492C4'
+    submitBtn.style.border='none'
 
   };
 
